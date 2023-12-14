df5021d4462d951d190783ac2f0c690932a79983
mithil <mithilsalunkhe@outlook.com>
2023-12-14 23:57:15 +0530

    small test

--- finetune.py
+++ finetune.py
@@ -11,9 +11,9 @@ import os
 model = "openai/whisper-tiny"
 common_voice = DatasetDict()
 
-common_voice["train"] = load_dataset("mozilla-foundation/common_voice_11_0", "hbs", split="train+validation",
+common_voice["train"] = load_dataset("mozilla-foundation/common_voice_11_0", "hsb", split="train+validation",
                                      token=True, ).select(range(100))
-common_voice["test"] = load_dataset("mozilla-foundation/common_voice_11_0", "hbs", split="test", token=True,
+common_voice["test"] = load_dataset("mozilla-foundation/common_voice_11_0", "hsb", split="test", token=True,
                                     ).select(range(100))
 #
 common_voice = common_voice.remove_columns(
