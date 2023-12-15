9fa1c848025cf09335b3c96325c1226d5d5ef38a
mithil <mithilsalunkhe@outlook.com>
2023-12-15 17:38:25 +0530

    switching to streaming

--- finetune.py
+++ finetune.py
@@ -27,6 +27,8 @@ tokenizer = WhisperTokenizer.from_pretrained(model, language="Chinese", task="tr
 feature_extractor = WhisperFeatureExtractor.from_pretrained(model)
 processor = WhisperProcessor(feature_extractor=feature_extractor, tokenizer=tokenizer)
 
+common_voice = common_voice.remove_columns(
+    ["accent", "age", "client_id", "down_votes", "gender", "locale", "path", "segment", "up_votes"])
 
 class CFG:
     num_devices = torch.cuda.device_count()
@@ -47,7 +49,50 @@ def prepare_dataset(batch):
     return batch
 
 
-common_voice = common_voice.map(prepare_dataset, )
+common_voice = common_voice.map(prepare_dataset,remove_columns=common_voice.column_names["train"] )
+
+
+from datasets import load_dataset, DatasetDict
+from datasets import Audio
+from transformers import WhisperProcessor, WhisperFeatureExtractor, WhisperTokenizer
+import torch
+import evaluate
+
+from dataclasses import dataclass
+from typing import Any, Dict, List, Union
+import os
+
+model = "openai/whisper-tiny"
+common_voice = DatasetDict()
+
+common_voice["train"] = load_dataset("mozilla-foundation/common_voice_11_0", "hsb", split="train+validation",
+                                     token=True, ).select(range(100))
+common_voice["test"] = load_dataset("mozilla-foundation/common_voice_11_0", "hsb", split="test", token=True,
+                                    ).select(range(100))
+#
+common_voice = common_voice.remove_columns(
+    ["accent", "age", "client_id", "down_votes", "gender", "locale", "path", "segment", "up_votes"])
+
+common_voice = common_voice.cast_column("audio", Audio(sampling_rate=16000))
+
+tokenizer = WhisperTokenizer.from_pretrained(model, language="Chinese", task="transcribe")
+feature_extractor = WhisperFeatureExtractor.from_pretrained(model)
+processor = WhisperProcessor(feature_extractor=feature_extractor, tokenizer=tokenizer)
+
+
+def prepare_dataset(batch):
+    # load and resample audio data from 48 to 16kHz
+    audio = batch["audio"]
+
+    # compute log-Mel input features from input audio array
+    batch["input_features"] = feature_extractor(audio["array"], sampling_rate=audio["sampling_rate"]).input_features[0]
+
+    # encode target text to label ids
+    batch["labels"] = tokenizer(batch["sentence"]).input_ids
+    return batch
+
+
+#common_voice = common_voice.map(prepare_dataset, remove_columns=common_voice.column_names["train"],)
 
 
 @dataclass
@@ -79,6 +124,7 @@ class DataCollatorSpeechSeq2SeqWithPadding:
 
 
 data_collator = DataCollatorSpeechSeq2SeqWithPadding(processor=processor)
+
 
 metric = evaluate.load("wer")
 
@@ -121,7 +167,6 @@ training_args = Seq2SeqTrainingArguments(
     push_to_hub=False,
     save_strategy="steps",
     dataloader_pin_memory=True,
-    dataloader_num_workers=8,
     eval_steps=40000 // CFG.batch_size,
     save_steps=40000 // CFG.batch_size,
     max_steps=int(40000 * CFG.epochs // CFG.batch_size),
