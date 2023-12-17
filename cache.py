ea434f4f36f1e99e928f0afc0d3434f2289f52c9
mithil <mithilsalunkhe@outlook.com>
2023-12-17 13:09:15 +0530

    new cache.py

--- cache.py
+++ cache.py
@@ -1,6 +1,6 @@
 from datasets import load_dataset
 
-dataset_name = "mozilla-foundation/common_voice_11_0"
+dataset_name = "mozilla-foundation/common_voice_13_0"
 split_name = "zh-CN"
 
 dataset = load_dataset(dataset_name, split_name)
