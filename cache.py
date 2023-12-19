301ec4eb11a883d5c4c73c0bdb8608c24e5ca393
mithil <mithilsalunkhe@outlook.com>
2023-12-19 11:16:38 +0530

    restore to a100 batch size

--- cache.py
+++ cache.py
@@ -3,4 +3,4 @@ from datasets import load_dataset
 dataset_name = "mozilla-foundation/common_voice_13_0"
 split_name = "zh-CN"
 
-dataset = load_dataset(dataset_name, split_name)
+dataset = load_dataset(dataset_name, split_name,num_proc=8)
