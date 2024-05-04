102173d762764eb0a9ac3faf3b040b654dae46ff
mithil <mithilsalunkhe@outlook.com>
2024-05-04 11:01:46 +0530

    basic text norm

--- finetuning_custom.py
+++ finetuning_custom.py
@@ -12,30 +12,28 @@ from torch.utils.data import DataLoader
 from torch.utils.data import Dataset
 from tqdm import tqdm
 from transformers import WhisperProcessor, WhisperFeatureExtractor, WhisperTokenizer, WhisperForConditionalGeneration, \
-    get_linear_schedule_with_warmup, Wav2Vec2Processor, Wav2Vec2FeatureExtractor, Wav2Vec2Tokenizer, \
-    Wav2Vec2ForCTC
+    get_linear_schedule_with_warmup
+
 from transformers.models.whisper.english_normalizer import BasicTextNormalizer
 
 accelerate = Accelerator(log_with=['wandb'], mixed_precision="bf16", gradient_accumulation_steps=2)
 accelerate.init_trackers(project_name="SR-Finetuning-custom")
-model = "jonatasgrosman/wav2vec2-large-xlsr-53-chinese-zh-cn"
+model = "openai/whisper-large-v3"
 common_voice = IterableDatasetDict()
-tokenizer = Wav2Vec2Tokenizer.from_pretrained(model, language="Chinese", task="transcribe")
-feature_extractor = Wav2Vec2FeatureExtractor.from_pretrained(model)
-processor = Wav2Vec2Processor(feature_extractor=feature_extractor, tokenizer=tokenizer)
+# Initialize the model and optimizer
+tokenizer = WhisperTokenizer.from_pretrained(model, language="Chinese", task="transcribe")
+feature_extractor = WhisperFeatureExtractor.from_pretrained(model)
+processor = WhisperProcessor(feature_extractor=feature_extractor, tokenizer=tokenizer)
 
-model = Wav2Vec2ForCTC.from_pretrained(model, )
+model = WhisperForConditionalGeneration.from_pretrained(model, )
 model.config.forced_decoder_ids = None
 model.config.suppress_tokens = []
 model.gradient_checkpointing_enable()
 model.use_cache = False
-#model.generation_config.language = "zh"
+model.generation_config.language = "zh"
 optimizer = AdamW8bit(model.parameters(), lr=1e-5)
 
-
 normalizer = BasicTextNormalizer()
-
-
 class CFG:
     num_devices = torch.cuda.device_count()
     batch_size = 8
@@ -44,79 +42,35 @@ class CFG:
     num_workers = 8
 
 
-import torch
-
-from dataclasses import dataclass, field
-from typing import Any, Dict, List, Optional, Union
-
-
-class DataCollatorCTCWithPadding:
-    """
-    Data collator that will dynamically pad the inputs received.
-    Args:
-        processor (:class:`~transformers.Wav2Vec2Processor`)
-            The processor used for proccessing the data.
-        padding (:obj:`bool`, :obj:`str` or :class:`~transformers.tokenization_utils_base.PaddingStrategy`, `optional`, defaults to :obj:`True`):
-            Select a strategy to pad the returned sequences (according to the model's padding side and padding index)
-            among:
-            * :obj:`True` or :obj:`'longest'`: Pad to the longest sequence in the batch (or no padding if only a single
-              sequence if provided).
-            * :obj:`'max_length'`: Pad to a maximum length specified with the argument :obj:`max_length` or to the
-              maximum acceptable input length for the model if that argument is not provided.
-            * :obj:`False` or :obj:`'do_not_pad'` (default): No padding (i.e., can output a batch with sequences of
-              different lengths).
-        max_length (:obj:`int`, `optional`):
-            Maximum length of the ``input_values`` of the returned list and optionally padding length (see above).
-        max_length_labels (:obj:`int`, `optional`):
-            Maximum length of the ``labels`` returned list and optionally padding length (see above).
-        pad_to_multiple_of (:obj:`int`, `optional`):
-            If set will pad the sequence to a multiple of the provided value.
-            This is especially useful to enable the use of Tensor Cores on NVIDIA hardware with compute capability >=
-            7.5 (Volta).
-    """
-
-    processor: Wav2Vec2Processor = processor
-    padding: Union[bool, str] = True
-    max_length: Optional[int] = None
-    max_length_labels: Optional[int] = None
-    pad_to_multiple_of: Optional[int] = None
-    pad_to_multiple_of_labels: Optional[int] = None
+@dataclass
+class DataCollatorSpeechSeq2SeqWithPadding:
+    processor: Any
 
     def __call__(self, features: List[Dict[str, Union[List[int], torch.Tensor]]]) -> Dict[str, torch.Tensor]:
-        # split inputs and labels since they have to be of different lengths and need
-        # different padding methods
-        input_features = [{"input_values": feature["input_values"]} for feature in features]
-        label_features = [{"input_ids": feature["labels"]} for feature in features]
+        # split inputs and labels since they have to be of different lengths and need different padding methods
+        # first treat the audio inputs by simply returning torch tensors
+        input_features = [{"input_features": feature["input_features"]} for feature in features]
+        batch = self.processor.feature_extractor.pad(input_features, return_tensors="pt", )
 
-        batch = self.processor.pad(
-            input_features,
-            padding=self.padding,
-            max_length=self.max_length,
-            pad_to_multiple_of=self.pad_to_multiple_of,
-            return_tensors="pt",
-        )
-        with self.processor.as_target_processor():
-            labels_batch = self.processor.pad(
-                label_features,
-                padding=self.padding,
-                max_length=self.max_length_labels,
-                pad_to_multiple_of=self.pad_to_multiple_of_labels,
-                return_tensors="pt",
-            )
+        # get the tokenized label sequences
+        label_features = [{"input_ids": feature["labels"]} for feature in features]
+        # pad the labels to max length
+        labels_batch = self.processor.tokenizer.pad(label_features, return_tensors="pt")
 
         # replace padding with -100 to ignore loss correctly
         labels = labels_batch["input_ids"].masked_fill(labels_batch.attention_mask.ne(1), -100)
+
+        # if bos token is appended in previous tokenization step,
+        # cut bos token here as it's append later anyways
+        if (labels[:, 0] == self.processor.tokenizer.bos_token_id).all().cpu().item():
+            labels = labels[:, 1:]
 
         batch["labels"] = labels
 
         return batch
 
 
-
-
-data_collator = DataCollatorCTCWithPadding()
-
-
+data_collator = DataCollatorSpeechSeq2SeqWithPadding(processor=processor)
 
 common_voice["train"] = load_dataset("Mithilss/L2TS",
                                      split="train",
@@ -130,9 +84,8 @@ common_voice["train"] = common_voice["train"].shuffle(seed=42)
 
 
 class WhisperDataset(Dataset):
-    def __init__(self, dataset: IterableDatasetDict, augmentation=None):
+    def __init__(self, dataset: IterableDatasetDict, ):
         self.dataset = dataset
-        self.augmentation = augmentation
 
     def __len__(self):
         return len(self.dataset)
@@ -141,11 +94,11 @@ class WhisperDataset(Dataset):
         audio = batch["audio"]
 
         # compute log-Mel input features from input audio array
-        batch["input_values"] = \
-            feature_extractor(audio["array"], sampling_rate=audio["sampling_rate"]).input_values[0]
-        batch['transcript'] = normalizer(batch['transcript'])
+        batch["input_features"] = \
+            feature_extractor(audio["array"], sampling_rate=audio["sampling_rate"]).input_features[0]
+
         # encode target text to label ids
-        batch["labels"] = tokenizer(batch["transcript"], truncation=True, max_length=256,
+        batch["labels"] = tokenizer(batch["transcript"], truncation=True, max_length=448,
                                     padding="max_length").input_ids
         return batch
 
